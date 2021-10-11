63f03e7b18087f6a48121155c8e4cad6c0b92dde
Federico Garcia <fgarcia1012002@gmail.com>
2021-10-11 17:56:19 -0300

    Added ArgParse to control BurnIn and Samples. Added Title and XYlabels controls.

--- README.md
+++ README.md
@@ -3,14 +3,27 @@
 A Python tool to make interactive [Corner Plots](https://corner.readthedocs.io/) based on XSPEC MCMC chains saved to FITS files.
 
 ### Usage:
+
+**Basic:** The user must provide the path/name to the MCMC XSPEC chain FITS file.
 ```
    python pyXspecCorner.py 'MCMC_chain.fits'
+```
+
+**Optional:** The user can also provide the burn in and/or number of samples to use for plotting purposes.
+
+```
+   python pyXspecCorner.py 'MCMC_chain.fits' burn 300 samples 1000
+```
+
+**Usage help:** The user can get additional description/help using the argparse helper.
+```
+   python pyXspecCorner.py -h
 ```
 
 ### Description:
 
 The program will open two matplotlib windows, one with a corner plot and another one with a list of CheckButtons to interactively select the parameters to be plotted in the corner plot. 
 
-The last CheckButton in the list allows to turn on and off the smoothed 2D contours in the [Corner Plot](https://corner.readthedocs.io/).
+The lasts CheckButtons in the list allows to turn on and off the smoothed 2D contours in the [Corner Plot](https://corner.readthedocs.io/), and to play with Titles and XYlabels in the plot.
 
 
--- pyXspecCorner.py
+++ pyXspecCorner.py
@@ -1,4 +1,4 @@
-import sys
+import argparse
 import numpy as np
 import pandas as pd
 import arviz
@@ -7,96 +7,126 @@ from matplotlib.widgets import CheckButtons
 from astropy.io import fits
 import corner
 
+
+# Use very very small fonts...
 plt.rc('xtick',labelsize='xx-small')
 plt.rc('ytick',labelsize='xx-small')
 
-chainName = sys.argv[-1]
-maxSamples = int(1e4)
 
-chain = fits.open(chainName)
-nFields = chain[1].header['TFIELDS']
-ChainLength = chain[1].header['NAXIS2']
-idx = np.random.choice(ChainLength,int(min(maxSamples,ChainLength)))
+def UpdateCornerPlot(selectedTitles, contours, showTitles, showXYlabels):
+    '''Create and Update the CornerPlot based on the selectedTitles,
+       and plotting options like Contours, Titles and Labels'''
+    figcorner.clear()
+    if showXYlabels:
+        corner.corner(df, var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
+                  labels=[''.join(title.split('. ')[1:]) for title in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
+                  titles=selectedTitles.values, show_titles=showTitles, title_kwargs={"fontsize": 'xx-small'},
+                  plot_datapoints=False, plot_density=True, plot_contours=contours, smooth=True,
+                  quantiles=(0.14, 0.84), use_math_text=True)
+    else:
+        corner.corner(df, var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
+                  labels=[None for val in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
+                  titles=selectedTitles.values, show_titles=showTitles, title_kwargs={"fontsize": 'xx-small'},
+                  plot_datapoints=False, plot_density=True, plot_contours=contours, smooth=True,
+                  quantiles=(0.14, 0.84), use_math_text=True)
+   
+    figcorner.canvas.draw()
+    return
+
 
 def chButtonsFunc(a):
+    '''Read Selected Buttons and seamingly call
+       UpdateCornerPlot to update the corner plot'''
     for i, Title in enumerate(Titles):
         if a == Title:
             selected[i] = not selected[i]
             break
-    contours = selected[-1]
+    contours = selected[-3]
+    showTitles = selected[-2]
+    showXYlabels = selected[-1]
     selectedTitles = Titles[selected]
 
-    figcorner.clear()
-    if contours:
-        corner.corner(df.loc[idx], var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
-                  labels=[None for val in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
-                  titles=selectedTitles.values, show_titles=True, title_kwargs={"fontsize": 'xx-small'},
-                  plot_datapoints=False, plot_density=True, plot_contours=True, smooth=True)
-    else:    
-        corner.corner(df.loc[idx], var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
-                  labels=[None for val in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
-                  titles=selectedTitles.values, show_titles=True, title_kwargs={"fontsize": 'xx-small'},
-                  plot_datapoints=False, plot_density=True, plot_contours=False, no_fill_contours=True)
-
-    figcorner.canvas.draw()
-
+    UpdateCornerPlot(selectedTitles, contours, showTitles, showXYlabels)
     return 
-
-
-df = pd.DataFrame()
-titles, ttypes = [], []
-
-for i in range(nFields):
-    ttype = chain[1].header['TTYPE{}'.format(i+1)]
-    tform = chain[1].header['TFORM{}'.format(i+1)]
-    try:
-        tunit = chain[1].header['TUNIT{}'.format(i+1)]
-    except:
-        tunit = ''
     
-    try:
-        tname, tnum = ttype.split('__')
-    except:
-        tname = 'Chi-Squared'
-        tunit = ''
 
-    ttypes.append(ttype)
-    if tunit:
-        title = '{}. {} [{}]'.format(tnum,tname,tunit)
-    else:
-        title = '{}. {}'.format(tnum,tname)
+if __name__ == '__main__':
+    '''pyXspecCorner is a CornerPlotter for XSPEC MCMC Chains saved to FITS files'''
+    
+    # Organize the Parser to get Chain file, burn-in and samples to be used.
+    parser = argparse.ArgumentParser(prog='pyXspecCorner',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                    description='Make interactive CornerPlots based on XSPEC MCMC Chain FITS files.')
+
+    parser.add_argument("chain", help="Path to XSPEC Chain FITS file", type=str, default='chain.fits')
+    parser.add_argument("--burn", help="Samples to Burn In", type=int, default=100, nargs='?')
+    parser.add_argument("--samples",help="Samples used in CornerPlot", type=int, default=1000, nargs='?')
+    args = parser.parse_args()
+
+    # Use the parsed arguments to get the selected data from the Chain FITS file
+    chainName = args.chain
+    BurnIn = int(args.burn)
+    Samples = int(args.samples)
+
+    chain = fits.open(chainName)
+    nFields = chain[1].header['TFIELDS']
+    ChainLength = chain[1].header['NAXIS2']
+
+    idx = np.random.randint(low=int(min(BurnIn,ChainLength//2)), high=ChainLength, size=int(min(Samples,ChainLength)))
+
+    # Create the DataFrame for the CornerPlot and fill it with the Data and Titles.
+    df = pd.DataFrame()
+    titles, ttypes = [], []
+
+    for i in range(nFields):
+        ttype = chain[1].header['TTYPE{}'.format(i+1)]
+        tform = chain[1].header['TFORM{}'.format(i+1)]
+        try:
+            tunit = chain[1].header['TUNIT{}'.format(i+1)]
+        except:
+            tunit = ''
         
-    titles.append(title)
-    df[title] = chain[1].data[ttype]
+        try:
+            tname, tnum = ttype.split('__')
+        except:
+            tname = 'Chi-Squared'
+            tunit = ''
 
-titles.append('Draw Contours')
-contours = False
+        ttypes.append(ttype)
+        if tunit:
+            title = '{}. {} [{}]'.format(tnum,tname,tunit)
+        else:
+            title = '{}. {}'.format(tnum,tname)
+            
+        titles.append(title)
+        df[title] = chain[1].data[ttype][idx]
 
-figcorner = plt.figure()
-figbuttons, ax = plt.subplots(1)
+    # Add some Plotting abilities
+    titles.append('Draw Contours')
+    contours = False
+    titles.append('Show Titles')
+    showTitles = True
+    titles.append('Show XY Labels')
+    showXYlabels = False
 
-Titles = pd.Series(titles)
-selected = [False for Title in Titles]
-selected[-2] = True
-selectedTitles = Titles[selected]
+    # Make the Titles and Pre-select Chi-square and Titles
+    Titles = pd.Series(titles)
+    selected = [False for Title in Titles]
+    selected[-4] = True
+    selected[-2] = True
+    selectedTitles = Titles[selected]
 
-chButtons = CheckButtons(ax, Titles, actives=selected)
-chButtons.on_clicked(chButtonsFunc)
-figbuttons.suptitle('Parameters /// Draw Contours')
+    # Create the two interactive figures and fill them: Buttons and CornerPlot
+    figcorner = plt.figure()
+    figbuttons, ax = plt.subplots(1)
 
-if contours:
-    corner.corner(df.loc[idx], var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
-              labels=[None for val in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
-              titles=selectedTitles.values, show_titles=True, title_kwargs={"fontsize": 'xx-small'},
-              plot_datapoints=False, plot_density=True, plot_contours=True, smooth=True)
-else:    
-    corner.corner(df.loc[idx], var_names=selectedTitles.values, filter_vars="like", fig=figcorner,
-              labels=[None for val in selectedTitles.values], label_kwargs={"fontsize": 'xx-small'},
-              titles=selectedTitles.values, show_titles=True, title_kwargs={"fontsize": 'xx-small'},
-              plot_datapoints=False, plot_density=True, plot_contours=False, no_fill_contours=True)
-figcorner.canvas.draw()
+    chButtons = CheckButtons(ax, Titles, actives=selected)
+    chButtons.on_clicked(chButtonsFunc)
+    figbuttons.suptitle('Parameters /// Draw Contours')
 
-plt.tight_layout()
-plt.show()
+    UpdateCornerPlot(selectedTitles, contours, showTitles, showXYlabels)
+
+    plt.tight_layout()
+    plt.show()
 
 
